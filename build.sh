da9bbd7e41a4d2f6713c0c5953318763b1c0e301
Adrien MISSEMER <adrien.missemer@hybris.com>
2016-08-24 10:51:39 -0400

    created the swarm variant

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 docker build --pull -t amissemer/git-sync-jenkins:2.7.2 $DIR
+docker build --pull -t amissemer/git-sync-jenkins:2.7.2-swarm -f Dockerfile-swarm $DIR
